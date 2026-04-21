dfa5eaea77fa996860802aa3e0beaf714f1b49de
m-tky <takuya.mukai@gmail.com>
2026-04-22 01:44:48 +0900

    chore: removed extra files

--- lua/Default/ai/init.lua
+++
@@ -1,9 +0,0 @@
-require("lze").load({
-  {
-    "copilot.lua",
-    after = function()
-      require("copilot").setup({ panel = { enabled = false }, suggestion = { enabled = false } })
-    end,
-    event = { "InsertEnter", "CmdlineEnter" },
-  },
-})
